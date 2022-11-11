ba77f8976a250d7fcbb20c7c59718120a30b2f20
Kanghj <rkdguswns0803@gmail.com>
2022-11-11 17:49:02 +0900

    footer 1

---
+++ react_project/src/components/Footer.jsx
@@ -0,0 +1,38 @@
+import "../css/footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <table className="">
+        <tbody>
+          <tr>
+            <td className="">
+              <strong>상호명</strong>
+            </td>
+            <td className="">마르꾸파리 센텀점</td>
+          </tr>
+          <tr>
+            <td className="">
+              <strong>사업자 번호&nbsp;</strong>
+            </td>
+            <td className="">492-09-00274</td>
+          </tr>
+          <tr>
+            <td className="">
+              <strong>전화번호</strong>
+            </td>
+            <td className="">051-784-8799</td>
+          </tr>
+          <tr>
+            <td className="">
+              <strong>매장주소</strong>
+            </td>
+            <td className="">48057 부산광역시 해운대구 해운대로 219번길 21</td>
+          </tr>
+        </tbody>
+      </table>
+    </footer>
+  );
+};
+
+export default Footer;
--- react_project/src/pages/Layout.jsx
+++ react_project/src/pages/Layout.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
 import NavbarComp from "../components/NavbarComp";
 
 const Layout = () => {
   return (
     <div>
       <NavbarComp />
-
       <Outlet />
+      <Footer />
     </div>
   );
 };
